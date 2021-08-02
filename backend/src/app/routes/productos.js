d1d0c9c4946398997a8a9068c308d08fa559dd6c
celestebatres <celestebatres16@gmail.com>
2021-08-01 22:15:45 -0600

    requestsBackend

--- backend/src/app/routes/productos.js
+++ backend/src/app/routes/productos.js
@@ -2,6 +2,7 @@ const conn = require("../../config/database"); //Necesitamos acceder a la base d
 
 module.exports = (app) => {
 
+    //GET
     app.get('/productos', (req, res, next)=> {
         let consulta = "SELECT * FROM productos"; 
         conn.query(consulta, (err, rows, cols) =>{
@@ -9,6 +10,43 @@ module.exports = (app) => {
                 res.status(500).json({status: 0, mensaje: "Error"});
             } else {
                 res.json({status: 1, mensaje: "Exitoso", productos: rows})
+            }
+        });
+    });
+
+    //POST --- También se guarda un id (Código)
+    app.post('/productos', (req, res, next)=> {
+        let consulta = "INSERT INTO productos (id_producto, nombre, precio, grms, existencia, fecha_venc) VALUES ( '" + req.body.id_producto + "','"  + req.body.nombre + "','" + req.body.precio + "','" + req.body.grms+ "','" + req.body.existencia + "','" + req.body.fecha_venc + "')"; 
+        conn.query(consulta, (err, rows, cols) =>{
+            if(err){
+                res.status(500).json({status: 0, mensaje: "Error"});
+            } else {
+                res.json({status: 1, mensaje: "Exitoso"})
+            }
+        });
+    });
+
+    //PUT
+    app.put('/productos/:id', (req, res, next)=>{
+        let consulta = "UPDATE productos SET nombre = '" + req.body.nombre + "', precio = '" + req.body.precio + "', grms = '" + req.body.grms + "', existencia = '" + req.body.existencia + "', fecha_venc = '" + req.body.fecha_venc + "'WHERE id_producto = " + req.params.id;
+        
+        conn.query(consulta, (err, rows, cols) => {
+            if(err){
+                res.status(500).json({status: 0, mensaje: "Error"});
+            }else{
+                res.json({mensaje: "Exitoso"});
+            }
+        });
+    });
+
+    //DELETE
+    app.delete('/productos/:id', (req, res, next) =>{
+        let consulta = "DELETE FROM productos WHERE id_producto = "+ req.params.id;
+        conn.query(consulta, (err, rows, cols) => {
+            if(err){
+                res.status(500).json({status: 0, mensaje: "Error"});
+            } else{
+                res.json({mensaje: "Exitoso"});
             }
         });
     });
